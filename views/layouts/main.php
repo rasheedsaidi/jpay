aff052001393be28fa28b2af35029f845c11ccec
Adegoke Saidi A <rasheedsaidi@gmail.com>
2017-04-27 15:03:58 +0100

    Add Update

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -18,7 +18,7 @@ AppAsset::register($this);
     <meta charset="<?= Yii::$app->charset ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?= Html::csrfMetaTags() ?>
-    <title><?= Html::encode($this->title) ?></title>
+    <title><?= "Assessment Test" ?></title>
     <?php $this->head() ?>
 </head>
 <body>
